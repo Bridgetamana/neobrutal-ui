190a69df0aa296cbb65e399a4bcbe35d29001caa
Bridgetamana <amanabridget03@gmail.com>
2026-02-22 13:18:35 +0100

    feat: migrate documentation from TSX to MDX and update changelog structure

--- .source/browser.ts
+++ .source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"accessibility.mdx": () => import("../content/docs/accessibility.mdx?collection=docs"), "cli.mdx": () => import("../content/docs/cli.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"accessibility.mdx": () => import("../content/docs/accessibility.mdx?collection=docs"), "changelog.mdx": () => import("../content/docs/changelog.mdx?collection=docs"), "cli.mdx": () => import("../content/docs/cli.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "installation.mdx": () => import("../content/docs/installation.mdx?collection=docs"), "theming.mdx": () => import("../content/docs/theming.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- .source/server.ts
+++ .source/server.ts
@@ -1,6 +1,9 @@
 // @ts-nocheck
-import * as __fd_glob_2 from "../content/docs/index.mdx?collection=docs"
-import * as __fd_glob_1 from "../content/docs/cli.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/theming.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/installation.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/cli.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/changelog.mdx?collection=docs"
 import * as __fd_glob_0 from "../content/docs/accessibility.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
@@ -10,4 +13,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {}, {"accessibility.mdx": __fd_glob_0, "cli.mdx": __fd_glob_1, "index.mdx": __fd_glob_2, });
+export const docs = await create.docs("docs", "content/docs", {}, {"accessibility.mdx": __fd_glob_0, "changelog.mdx": __fd_glob_1, "cli.mdx": __fd_glob_2, "index.mdx": __fd_glob_3, "installation.mdx": __fd_glob_4, "theming.mdx": __fd_glob_5, });
