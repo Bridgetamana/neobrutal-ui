ed7fe472d46deea7adbf8454958ad97e147133c0
Bridgetamana <amanabridget03@gmail.com>
2026-02-27 16:26:04 +0100

    update introduction docs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -8,6 +8,12 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...defaultMdxComponents,
     ChangelogContent,
     Preview,
+    h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h2 className="text-2xl font-bold mt-10 mb-4 text-black border-0 pb-0" {...props} />
+    ),
+    h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h3 className="text-xl font-bold mt-8 mb-3 text-black border-0 pb-0" {...props} />
+    ),
     ...components,
   };
 }
